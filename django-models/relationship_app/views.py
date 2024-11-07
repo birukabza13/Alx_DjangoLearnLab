6ebfa71dc22d5c41bfc2656732fbce99c8fed3b0
birukabza <birukabza@gmail.com>
2024-11-07 16:50:40 +0300

    updated the relationship_app/views.py to have a login, logout, registration view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Library
 from .models import Book
 from django.views.generic.detail import DetailView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 
 class LibraryDetailView(DetailView):
@@ -14,3 +18,30 @@ def list_books(request):
     book = Book.objects.all()
     context = {"books": book}
     return render(request, "relationship_app/list_books.html", context)
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
