0264862eb71554f8317bbef4117e51053cba640c
birukabza <birukabza@gmail.com>
2024-11-07 19:47:03 +0300

    testing

--- django-models/admin_view.py
+++ django-models/admin_view.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
+
 def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == "Admin"
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+
 
 @user_passes_test(is_admin)
-def Admin(request):
+def admin_view(request):
     return render(request)
