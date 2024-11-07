156cb6bb60cb1fc9fb4d83f1b281a5c3cdc24037
birukabza <birukabza@gmail.com>
2024-11-07 16:52:06 +0300

    updated the relationship_app/views.py to have a login, logout, registration view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,7 +4,8 @@ from .models import Book
 from django.views.generic.detail import DetailView
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
 
