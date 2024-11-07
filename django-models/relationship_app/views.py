cfa182255d238086c6b9e123a8e5607981798a93
birukabza <birukabza@gmail.com>
2024-11-07 16:02:31 +0300

    added a class based view in 'relationship_app/views to list all books in a library

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Library
+from django.views.generic import DetailView
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
 
 def list_books(request):
     book = Book.objects.all()
-
-    context = {
-        "books": book
-    }
-    
-    return render(request, 'relationship_app/list_books.html', context)
-
+    context = {"books": book}
+    return render(request, "relationship_app/list_books.html", context)
